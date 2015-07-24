8e0f88cf6bfaecd3c1dac59ebfda885b8eaf1c2d
Nct95 <sabreman65@hotmail.com>
2015-07-24 13:57:04 +1000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -24,21 +24,5 @@
     //this will get called every time the player touches the screen
     [character flap];
 }
--(type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName:(type of second parameter) secondParameter
--(void) doSomething
-{
-    NSLog(2"Hello World!");
 
-}
-
--(int) doSomethingWithAString: (NSString*) myString
-{
-    //returns the length of the string times ten
-    return [myString length] * 10;
-}
-
--(NSArray *) addThisStringToAnArray:(NSString*) firstString andThisString:(NSString*) secondString andAlsoThisString:(NSString*) thirdString
-{
-    return [NSArray arrayWithObjects: firstString, secondString, thirdString, nil];
-}
 @end
