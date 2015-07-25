8f8af0ed5cf12c7a30d1b28ee789420f6622747a
Nct95 <sabreman65@hotmail.com>
2015-07-25 17:44:19 +1000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -23,6 +23,8 @@
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     //this will get called every time the player touches the screen
     [character flap];
+    [self addObstacle];
+    
 }
 
 @end
